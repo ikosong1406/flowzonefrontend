07949acc0ddc281b25aa15455b3daf4f9d9cc978
Alexandra <alexandervirtuous14@gmail.com>
2024-06-15 16:58:40 +0100

    modified:   src/pages/Accounts.jsx 	modified:   src/pages/Chat.jsx 	modified:   src/pages/Home.jsx 	new file:   src/styles/Accounts.css 	modified:   src/styles/Chat.css 	modified:   src/styles/Header.css 	modified:   src/styles/Home.css 	modified:   src/styles/Layout.css

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "../styles/Accounts.css";
 
 const Accounts = () => {
-  return <div>Accounts</div>;
+  return (
+    <div className="accountMain">
+      <div className="accountDiv1">
+        <h1>Profile</h1>
+      </div>
+      <div className="accountDiv2">
+        <div className="accountDiv21">1</div>
+        <div className="accountDiv22">2</div>
+      </div>
+    </div>
+  );
 };
 
 export default Accounts;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,22 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div className="homeMain">
-      <h1 style={{ color: "red" }}>HOME</h1>
+      <div className="homeDiv1">
+        <h1>Welcome, Alexander Virtuous Ikosong</h1>
+      </div>
+      <div className="homeDiv2">
+        <div className="homeDiv21">
+          <div className="homeDiv211">
+            <p>See what happening in your account</p>
+            <h3>Recent collaboration offers for you</h3>
+          </div>
+          <div className="homeDiv212"></div>
+        </div>
+        <div className="homeDiv22">
+          <div className="homeDiv221"></div>
+          <div className="homeDiv222"></div>
+        </div>
+      </div>
     </div>
   );
 };
